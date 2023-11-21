56728f59780d9dd09c745abddc09930cb4cfd878
redcodemohammed <redcodemohammed@gmail.com>
2023-11-21 15:35:57 +0300

    feat: added prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('api');
+
+  // swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Event Flow Hub API')
+    .setDescription('The Event Flow Hub description')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
